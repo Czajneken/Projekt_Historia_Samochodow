4f51380d9e656da03f09ba8e2c7dc16f1b21d695
Maciej Czaja <czaja.maciej.8@gmail.com>
2024-06-26 23:44:09 +0200

    start projektu

---
+++ Projekt_Historia_Samochodow/projekt_historia_samochodow/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ProjektHistoriaSamochodowConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'projekt_historia_samochodow'
